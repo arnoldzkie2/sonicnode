9ae9e6309714d52ca57ebfd5a8ab0980950b87b0
Arnold <arnoldzkie22@gmail.com>
2024-06-17 12:59:18 +0800

    fixed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,9 +4,11 @@ import { caller } from '../_trpc/server'
 import DashboardTabs from '@/components/dashboad/dashboard'
 import { getAuth } from '@/lib/nextauth'
 import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
 
 const Dashboard = async () => {
 
+    cookies()
     const session = await getAuth()
     if (!session) redirect('/auth')
     const dashboardData = await caller.dashboard.getDashboardData()
