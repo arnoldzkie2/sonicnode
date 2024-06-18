72d6d839a51843909ad039efe378eebae909637e
Arnold <arnoldzkie22@gmail.com>
2024-06-18 15:38:02 +0800

    tes

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
 
-    console.log(req.headers.get('x-forwarded-for'))
 
-    return NextResponse.json({ test: req })
+    const ip = req.headers.get('x-forwarded-for')
+
+    return NextResponse.json({ test: ip })
 }
